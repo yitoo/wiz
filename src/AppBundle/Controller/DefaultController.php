a3920a8f78789ef3b96cd90103016d335a3c1094
Abdoulaye BARRY <contact@abdoulayebarry.com>
2017-10-08 13:16:34 +0200

    Handle signup

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -17,11 +17,19 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/signup", name="signup", requirements={"method":"post'"})
+     * @Route("/praticien", name="practitioner")
+     */
+    public function practitionerAction()
+    {
+        return $this->render('default/practitioner.twig');
+    }
+
+    /**
+     * @Route("/practitioner-signup", name="practitioner_signup", requirements={"method":"post'"})
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
-    public function signUpAction(Request $request)
+    public function practitionerSignUpAction(Request $request)
     {
         $fullName = $request->request->get('fullName');
         $email = $request->request->get('email');
@@ -30,7 +38,7 @@ class DefaultController extends Controller
             ->setTo($email)
             ->setBody(
                 $this->renderView(
-                    ':emails:signup.html.twig',
+                    ':emails:practitioner_signup.html.twig',
                     array('name' => $fullName)
                 ),
                 'text/html'
@@ -40,6 +48,49 @@ class DefaultController extends Controller
 
         $this->addFlash('info', "Merci $fullName, nous vous avons envoyé un email à l'adresse $email");
 
-        return $this->redirectToRoute('homepage');
+        return $this->redirectToRoute('signup_success');
+    }
+
+    /**
+     * @Route("/entreprise", name="company")
+     */
+    public function companyAction()
+    {
+        return $this->render('default/company.twig');
+    }
+
+    /**
+     * @Route("/company-signup", name="company_signup", requirements={"method":"post'"})
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function companySignUpAction(Request $request)
+    {
+        $fullName = $request->request->get('company');
+        $email = $request->request->get('email');
+        $message = (new \Swift_Message('WorkInZen'))
+            ->setFrom('virginie@workinzen.fr', 'Virginie Plé')
+            ->setTo($email)
+            ->setBody(
+                $this->renderView(
+                    ':emails:company_signup.html.twig',
+                    array('name' => $fullName)
+                ),
+                'text/html'
+            )
+        ;
+        $this->get('mailer')->send($message);
+
+        $this->addFlash('info', "Merci $fullName, nous vous avons envoyé un email à l'adresse $email");
+
+        return $this->redirectToRoute('signup_success');
+    }
+
+    /**
+     * @Route("/votre-inscription", name="signup_success")
+     */
+    public function signUpSuccessAction()
+    {
+        return $this->render('default/signup_success.twig');
     }
 }
